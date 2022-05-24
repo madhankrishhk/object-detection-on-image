49d016156aa819e11f80dc33c3298da0b68ee967
madhankrishhk <84235355+madhankrishhk@users.noreply.github.com>
2022-05-24 16:41:08 +0530

    Add files via upload

---
+++ waterbottle.js
@@ -0,0 +1,48 @@
+img1 = "";
+status = "";
+objects = [];
+
+function preload() {
+    img1 = loadImage('Waterbottle.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw() {
+    image(img1, 0, 0, 640, 420);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) 
+        {
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+
+    }
+
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img1, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
